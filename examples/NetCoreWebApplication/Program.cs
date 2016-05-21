5a601279520081bf27d181cc7e158af2d0925f5e
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-05-21 13:40:44 +0200

    Add example NetCoreWebApplication

---
+++ examples/NetCoreWebApplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.AppDomain.NetCoreApp;
+using System.Diagnostics;
+
+namespace NetCoreWebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            Type thisType = typeof(Program);
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies(thisType);
+            foreach (var assembly in assemblies)
+            {
+                Debug.WriteLine(assembly.FullName);
+            }
+
+            host.Run();
+        }
+    }
+}
---
+++ examples/NetCoreWebApplication/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.AppDomain.NetCoreApp;
+
+namespace NetCoreWebApplication
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                Type thisType = typeof(Program);
+
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies(thisType);
+                await context.Response.WriteAsync(string.Join("\r\n", assemblies.Select(a => a.FullName)));
+            });
+        }
+    }
+}
