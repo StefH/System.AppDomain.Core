0b35e676c2721aa367b96e62eb52c97ee0b43a70
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2016-05-21 13:18:24 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # System.AppDomain.Core
-System.AppDomain.Core
+
+A replacement for `AppDomain.CurrentDomain.GetAssemblies()` call which is not persent for NetCoreApp/NetStandard.
+
+The package is available on [NuGet][1] : **Install-Package System.AppDomain.NetCoreApp**
+
+[1]: https://www.nuget.org/packages/System.AppDomain.NetCoreApp/ "NuGet - System.AppDomain.NetCoreApp"
---
+++ examples/NetCoreApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.AppDomain.NetCoreApp;
+
+namespace NetCoreApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("--start--");
+
+            Type thisType = typeof(Program);
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies(thisType);
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine(assembly.FullName);
+            }
+
+            Console.WriteLine("--end--");
+        }
+    }
+}
